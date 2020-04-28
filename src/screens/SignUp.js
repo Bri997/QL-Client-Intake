2fad23a4bd6e260cc104492092f18869189f25ac
Brian Musial <brian.musial@gmail.com>
2020-04-28 16:31:13 -0400

    adding aws amplify init for sign up page

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,28 +1,86 @@
 import React from 'react'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, useFormik } from 'formik'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import { CognitoUserPool } from 'amazon-cognito-js'
 
+import {
+  CognitoUserPool,
+  CognitoUserAttribute,
+  CognitoUser,
+} from 'amazon-cognito-identity-js'
+
+var AmazonCognitoIdentity = require('amazon-cognito-identity-js')
+// var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool
+
+//https://<your_domain>/login?response_type=code&client_id=<your_app_client_id>&redirect_uri=<your_callback_url>
+//https://the300group.auth.us-east-2.amazoncognito.com/login?response_type=code&client_id=6th7k23k4pflve9oa9u775jrhd&redirect_uri=https://www.the300group.net
 const SignUp = () => {
+  const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool
+
   const poolData = {
-    UserPoolId: 'us-east-2_SobCPAlji',
-    ClientId: '6th7k23k4pflve9oa9u775jrhd',
+    UserPoolId: 'us-east-2_iUKtX3Fr3',
+    ClientId: 'i8otj3ouft3tdncnkn62ue5bo',
+  }
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData)
+  const attributeList = []
+
+  const userData = {
+    UserName: 'brian',
+    Pool: userPool,
   }
 
-  // const UserPool = new CognitoUserPool(poolData)
+  var dataEmail = {
+    Name: 'email',
+    Value: 'email@mydomain.com',
+  }
+
+  var dataPhoneNumber = {
+    Name: 'phone_number',
+    Value: '+15555555555',
+  }
+  var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail)
+  var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(
+    dataPhoneNumber
+  )
+
+  attributeList.push(attributeEmail)
+  attributeList.push(attributePhoneNumber)
+
+  userPool.signUp('username', 'password', attributeList, null, function (
+    err,
+    result
+  ) {
+    if (err) {
+      alert(err.message || JSON.stringify(err))
+      return
+    }
+    var cognitoUser = result.user
+    console.log('user name is ' + cognitoUser.getUsername())
+  })
+
+  const formik = useFormik({
+    initialValues: { email: '', password: '', confirmPassword: '' },
+    onSubmit: (values) => {
+      const attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(
+        values.email
+      )
+      attributeList.push(attributeEmail)
+      // attributeList.push(attributePhoneNumber);
+      console.log(attributeList)
+      // alert(JSON.stringify(values, null, 2))
+      userPool.SignUp('email', 'password', [], null, (err, data) => {
+        if (err) alert(err.message || JSON.stringify(err))
+        console.log(err)
+        console.log('hey', data)
+      })
+    },
+  })
 
   return (
     <div>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-          confirmPassword: '',
-        }}
-      >
+      <Formik>
         {({ values, errors, isSubmitting }) => (
-          <Form>
+          <Form onSubmit={formik.handleSubmit}>
             <h2>Sign Up</h2>
             <Field
               variant="outlined"
@@ -35,28 +93,36 @@ const SignUp = () => {
               placeholder="Email"
               autoComplete="email"
               as={TextField}
+              onChange={formik.handleChange}
+              value={formik.values.email}
             />
             <Field
               variant="outlined"
               required
               fullWidth
-              name="password"
               label="Password"
               type="password"
               id="password"
+              name="password"
+              placeholder="Password"
               autoComplete="current-password"
               as={TextField}
+              onChange={formik.handleChange}
+              value={formik.values.Password}
             />
             <Field
               variant="outlined"
               required
               fullWidth
-              name="confirmPassword"
               label="Confirm Password"
               type="password"
               id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm Email"
               autoComplete="confirm current-password"
               as={TextField}
+              onChange={formik.handleChange}
+              value={formik.confirmPassword}
             />
             <Button variant="contained" color="primary" type="submit">
               Submit
